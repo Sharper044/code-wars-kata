91c8aa1ac07b33155725410be6292ea0b923453d
Stuart Harper <sharper@overstock.com>
2019-01-03 15:58:49 -0700

    Completed RemoveZeros

--- 4kyu/RemoveZeros.js
+++ 4kyu/RemoveZeros.js
@@ -14,6 +14,25 @@ function removeZeros(array) {
 
 function moveToEnd(array, index) {
   for (let i = index; i < array.length - 1; i++) {
+    const tem = array[i];
+    array[i] = array[i + 1];
+    array[i + 1] = tem;
+  }
+  return array;
+}
+function removeZeros(array) {
+  for (let i = 0, len = array.length; i < len; i++) {
+    if (array[i] === 0 || array[i] === '0') {
+      array = moveToEnd(array, i);
+      len--;
+      i--;
+    }
+  }
+  return array;
+}
+
+function moveToEnd(array, index) {
+  for (let i = index; i < array.length - 1; i++) {
     const temp = array[i];
     array[i] = array[i + 1];
     array[i + 1] = temp;
