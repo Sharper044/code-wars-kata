12ed7f7bbae60444e4752383a5aca155baeaf730
Stuart Harper <sharper@overstock.com>
2019-01-14 12:42:09 -0700

    Completed SQLBasicsTruncating

---
+++ 7kyu/SQLBasicsTruncating_v1..sql
@@ -0,0 +1,7 @@
+
+            // SQLBasicsTruncating
+            // https://www.codewars.com//kata/594a8fa5a2db9e5f290000c3
+        
+            SELECT TRUNC(number1 + number2) towardzero
+FROM decimals;
+            
