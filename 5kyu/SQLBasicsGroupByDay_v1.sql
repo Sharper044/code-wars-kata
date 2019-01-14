44905e1cdfb2919bb52565f847bf849b2a4d4dfd
Stuart Harper <sharper@overstock.com>
2019-01-14 12:02:30 -0700

    Completed SQLBasicsGroupByDay

--- 5kyu/SQLBasicsGroupByDay_v1.sql
+++ 5kyu/SQLBasicsGroupByDay_v1.sql
@@ -1,8 +1,8 @@
 
-              // SQLBasicsGroupByDay
-              // https://www.codewars.com//kata/5811597e9d278beb04000038
+            // SQLBasicsGroupByDay
+            // https://www.codewars.com//kata/5811597e9d278beb04000038
         
-              SELECT created_at::timestamp::date as day, description, COUNT(description) as count
+            SELECT created_at::timestamp::date as day, description, COUNT(description) as count
 FROM events
 WHERE name = 'trained'
 GROUP BY description, created_at::timestamp::date
