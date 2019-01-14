b05257be6b3bb3e6114131f2583cccfb72a897e4
Stuart Harper <sharper@overstock.com>
2019-01-14 12:57:54 -0700

    Completed Arethenumbersinorder

---
+++ 7kyu/Arethenumbersinorder_v1.go
@@ -0,0 +1,20 @@
+
+            // Arethenumbersinorder
+            // https://www.codewars.com//kata/56b7f2f3f18876033f000307
+        
+            package kata
+
+func InAscOrder(numbers []int) bool {
+  if len(numbers) == 1 {
+    return true
+  } else {
+    for i := 1; i < len(numbers); i++ {
+      if numbers[i-1] > numbers[i] {
+        return false
+      }
+    }
+    return true
+  }
+}
+
+            
---
+++ 7kyu/Arethenumbersinorder_v1.js
@@ -0,0 +1,11 @@
+
+            // Arethenumbersinorder
+            // https://www.codewars.com//kata/56b7f2f3f18876033f000307
+        
+            function inAscOrder(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) return false;
+  }
+  return true;
+}
+            
