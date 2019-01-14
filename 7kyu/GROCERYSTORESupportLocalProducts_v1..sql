7078a2a5bbad9e4dab0f7da2e2fcc839563be902
Stuart Harper <sharper@overstock.com>
2019-01-14 12:42:09 -0700

    Completed GROCERYSTORESupportLocalProducts

---
+++ 7kyu/GROCERYSTORESupportLocalProducts_v1..sql
@@ -0,0 +1,11 @@
+
+            // GROCERYSTORESupportLocalProducts
+            // https://www.codewars.com//kata/5a8ed96bfd8c066e7f00011a
+        
+            SELECT COUNT(p.name) AS products, p.country AS country
+FROM products AS p
+WHERE country IN ('United States of America', 'Canada')
+GROUP BY country
+ORDER BY products DESC;
+
+            
