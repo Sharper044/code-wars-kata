38b03cf50e30e0ac84347f8c10bfa1c2bed02758
Stuart Harper <sharper@overstock.com>
2019-01-14 12:42:09 -0700

    Completed Multiply

---
+++ 8kyu/Multiply_v1..java
@@ -0,0 +1,10 @@
+
+            // Multiply
+            // https://www.codewars.com//kata/50654ddff44f800200000004
+        
+            public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
+            
---
+++ 8kyu/Multiply_v1..sql
@@ -0,0 +1,6 @@
+
+            // Multiply
+            // https://www.codewars.com//kata/50654ddff44f800200000004
+        
+            SELECT price * amount AS total FROM items
+            
