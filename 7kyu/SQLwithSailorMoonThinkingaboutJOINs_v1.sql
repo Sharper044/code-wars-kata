4e5f4663571d3ec810be2dc05b53dc65a28b410c
Stuart Harper <sharper@overstock.com>
2019-01-14 11:58:07 -0700

    Completed SQLwithSailorMoonThinkingaboutJOINs

---
+++ 7kyu/SQLwithSailorMoonThinkingaboutJOINs_v1.sql
@@ -0,0 +1,11 @@
+
+              // SQLwithSailorMoonThinkingaboutJOINs
+              // https://www.codewars.com//kata/5ab7a736edbcfc8e62000007
+        
+              SELECT sa.senshi_name AS sailor_senshi, sa.real_name_jpn AS real_name, c.name AS cat, sc.school
+FROM sailorsenshi AS sa
+LEFT JOIN cats AS c
+ON sa.cat_id = c.id
+LEFT JOIN schools AS sc
+ON sa.school_id = sc.id;
+            
