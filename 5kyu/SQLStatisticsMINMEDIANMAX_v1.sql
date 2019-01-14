394f658389cd8440cf167a4eee636a7b769bd0aa
Stuart Harper <sharper@overstock.com>
2019-01-14 12:02:30 -0700

    Completed SQLStatisticsMINMEDIANMAX

--- 5kyu/SQLStatisticsMINMEDIANMAX_v1.sql
+++ 5kyu/SQLStatisticsMINMEDIANMAX_v1.sql
@@ -1,8 +1,8 @@
 
-              // SQLStatisticsMINMEDIANMAX
-              // https://www.codewars.com//kata/58167fa1f544130dcf000317
+            // SQLStatisticsMINMEDIANMAX
+            // https://www.codewars.com//kata/58167fa1f544130dcf000317
         
-              SELECT MIN(score) AS min,
+            SELECT MIN(score) AS min,
   ROUND(PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY score)::numeric, 2)::float AS median,
   MAX(score) AS max
 FROM result;
