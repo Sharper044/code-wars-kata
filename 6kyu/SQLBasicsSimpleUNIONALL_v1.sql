42702b945d5e24d5b54e90195d4713fc3a425df8
Stuart Harper <sharper@overstock.com>
2019-01-14 12:02:30 -0700

    Completed SQLBasicsSimpleUNIONALL

--- 6kyu/SQLBasicsSimpleUNIONALL_v1.sql
+++ 6kyu/SQLBasicsSimpleUNIONALL_v1.sql
@@ -1,8 +1,8 @@
 
-              // SQLBasicsSimpleUNIONALL
-              // https://www.codewars.com//kata/58112f8004adbbdb500004fe
+            // SQLBasicsSimpleUNIONALL
+            // https://www.codewars.com//kata/58112f8004adbbdb500004fe
         
-              SELECT 'US' as location, *
+            SELECT 'US' as location, *
 FROM ussales
 WHERE price > 50.00
 UNION ALL
